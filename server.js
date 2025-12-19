33ab5b0a67016048584ebf93e334fd2400ad2833
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-12-19 13:42:38 +0530

    Add new CORS origin for production frontend

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(cors({
     "http://localhost:5500",
     "http://localhost:3000", 
     "http://127.0.0.1:5500",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://st-dashboard-frontend-pttt.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
